f0f1de5cffdaa847bf9e4c5a55a9963d07c2a01a
Bobomurod2004 <bobomurodgoibnazarov@gmail.com>
2026-04-10 16:03:20 +0500

    xatolar to'g'riladi

--- apps/news/urls.py
+++ apps/news/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 from .views import CategoryViewSet, NewsViewSet
 
-router = DefaultRouter()
+router = SimpleRouter()
 router.register('categories', CategoryViewSet)
 router.register('news', NewsViewSet)
 
